ba7d964e4c2ed717775fe257db4e0c8e6288fe21
Saurav Chamoli <sauravchamoli17@gmail.com>
2020-01-04 13:35:55 +0530

    First Commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var x = document.getElementById("error");
+  var y = document.getElementById("errormsg");
+  var z = document.getElementById("field");
+  var email = document.getElementById("email");
+  email.oninvalid = function(e) {
+    e.target.setCustomValidity("");
+    if (!e.target.validity.valid) {
+      x.style.visibility = "visible";
+      y.style.visibility = "visible";
+      z.style.border = "2px solid hsl(0, 93%, 68%)";
+    }
+  };
+  email.oninput = function(e) {
+    e.target.setCustomValidity("");
+  };
+});
